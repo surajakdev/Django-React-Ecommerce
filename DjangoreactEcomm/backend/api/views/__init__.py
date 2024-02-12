df5e1b34b90161ce29ced24263e27232d685c0b1
surajakdev <surajshrini97@gmail.com>
2024-02-13 00:13:44 +0530

    new commit

---
+++ DjangoreactEcomm/backend/api/views/__init__.py
@@ -0,0 +1,4 @@
+# this extra __init__.py is created to avoid import error
+
+# from api.views import product_views as views
+# ImportError: cannot import name 'product_views' from 'api.views' (D:\WebDev2022\django-basics\OnlineEcommerceDR\DjangoreactEcomm\backend\api\views.py)
